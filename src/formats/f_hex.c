706f4661db7af68e1faa668df4edb8b47f84abe6
Abe Augustijn <abe.augustijn@hotmail.nl>
2019-11-19 16:43:39 +0100

    Feat: started hex

--- src/formats/f_hex.c
+++ src/formats/f_hex.c
@@ -1,23 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   formats.h                                          :+:      :+:    :+:   */
+/*   f_hex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aaugusti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/12 14:58:28 by aaugusti          #+#    #+#             */
-/*   Updated: 2019/11/19 16:12:38 by aaugusti         ###   ########.fr       */
+/*   Created: 2019/11/19 16:11:57 by aaugusti          #+#    #+#             */
+/*   Updated: 2019/11/19 16:17:27 by aaugusti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FORMATS_H
-# define FORMATS_H
-# include <ft_printf.h>
+#include <ft_printf.h>
+#include <formats.h>
+#include <libft.h>
 
-int		f_string(t_format_info *info, va_list *args);
-int		f_char(t_format_info *info, va_list *args);
-int		f_int(t_format_info *info, va_list *args);
-int		f_hex(t_format_info *info, t_bool is_cap, va_list *args);
-int		f_percentage();
+int	f_hex(t_format_info *info, t_bool is_cap, va_list *args)
+{
+	unsigned int	to_put;
+	unsigned int	n_len;
 
-#endif
+	to_put = (unsigned int)va_arg(*args, unsigned int);
+	n_len = ft_ull_len((unsigned long long)to_put);
+}
