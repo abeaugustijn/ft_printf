70156fc1b4f41687d45f3a62b98dd6153c0480bd
Abe Augustijn <abe.augustijn@hotmail.nl>
2019-12-10 08:11:28 +0100

    Feat: used stdbool instead of t_bool

--- include/utils.h
+++ include/utils.h
@@ -6,20 +6,20 @@
 /*   By: aaugusti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/18 14:13:27 by aaugusti          #+#    #+#             */
-/*   Updated: 2019/12/09 12:00:42 by aaugusti         ###   ########.fr       */
+/*   Updated: 2019/12/10 08:10:56 by aaugusti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTILS_H
 # define UTILS_H
 # include <libft.h>
+# include <stdbool.h>
 
 void	ft_putchar_fd_count(char c, int fd, int *count);
 void	ft_putstr_fd_count(char *str, int fd, int *res);
 void	ft_putstr_n_fd_count(char *str, size_t n, int fd, int *res);
 void	ft_putnbr_ll_fd_count(long long int n, int fd, int *count);
 void	ft_putunsigned_fd_count(unsigned long long n, int fd, int *count);
-void	ft_puthex_fd_count(unsigned long long n, int fd, t_bool cap,
-		int *count);
+void	ft_puthex_fd_count(unsigned long long n, int fd, bool cap, int *count)
 
 #endif
--- src/utils/ft_puthex_fd_count.c
+++ src/utils/ft_puthex_fd_count.c
@@ -6,20 +6,21 @@
 /*   By: aaugusti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/07 11:13:25 by aaugusti          #+#    #+#             */
-/*   Updated: 2019/12/09 11:53:38 by aaugusti         ###   ########.fr       */
+/*   Updated: 2019/12/10 08:03:07 by aaugusti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <limits.h>
 #include <utils.h>
 #include <libft.h>
+#include <stdbool.h>
 
 /*
 **	Output the unsigned long long 'n' to 'fd' as a hexadecimal number. Also add
 **	the amount of printed chars to *'count'.
 */
 
-void	ft_puthex_fd_count(unsigned long long n, int fd, t_bool cap, int *count)
+void	ft_puthex_fd_count(unsigned long long n, int fd, bool cap, int *count)
 {
 	const char	*base = cap ? "0123456789ABCDEF" : "0123456789abcdef";
 
