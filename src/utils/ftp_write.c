95b91a90e5a37394939979470d306b708a3d6586
Abe Augustijn <abe.augustijn@hotmail.nl>
2020-06-16 18:10:41 +0200

    Hacky way of making this compile.
    Sould return -1 on write failure though...

--- src/utils/ftp_write.c
+++ src/utils/ftp_write.c
@@ -6,7 +6,7 @@
 /*   By: aaugusti <aaugusti@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/04/28 13:26:15 by aaugusti      #+#   #+#                  */
-/*   Updated: 2020/04/28 16:02:19 by aaugusti      ########   odam.nl         */
+/*   Updated: 2020/06/16 18:09:56 by aaugusti      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,19 @@
 
 void	ftp_write(char *to_write, char **target, size_t n, int *tot_written)
 {
+	int	write_return;
+
 	if (target)
 	{
 		ft_memcpy(*target, to_write, n);
 		(*target) += sizeof(char) * n;
 	}
 	else
-		write(FD, to_write, n);
+	{
+		write_return = write(FD, to_write, n);
+		if (write_return == -1)
+			return;
+	}
 	if (tot_written)
 		(*tot_written) += n;
 }
